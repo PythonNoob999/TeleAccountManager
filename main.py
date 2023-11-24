7799c85f668fe4a4dbacd63d4385973ee68845f4
PythonNoob999 <kerolisand@gmail.com>
2023-11-24 16:04:00 +0200

    fix

--- main.py
+++ main.py
@@ -14,15 +14,17 @@ async def main_handler(bot, m):
     txt = m.text.split()
     command = txt[0].replace("/", "")
 
-    if command in ["send_message", "join_chats","leave_chats", "ref","click","send_contact", "add_contact", "send_reaction"]:
-        kwargs = parse_kwargs(m.text, txt[0])
-        await m.reply("Executing⏰")
-        try:
-            count = pc()
-            result = await Execute(command, kwargs)
-            return await m.reply(f"Done {result['done']}/{result['total']}\nDone in {pc() - count} Seconds")
-        except Exception as e:
-            logger.exception(e)
-            return await m.reply("Invalid Arguments")
+    if user.id == bot.me.id:
+
+        if command in ["send_message", "join_chats","leave_chats", "ref","click","send_contact", "add_contact", "send_reaction"]:
+            kwargs = parse_kwargs(m.text, txt[0])
+            await m.reply("Executing⏰")
+            try:
+                count = pc()
+                result = await Execute(command, kwargs)
+                return await m.reply(f"Done {result['done']}/{result['total']}\nDone in {pc() - count} Seconds")
+            except Exception as e:
+                logger.exception(e)
+                return await m.reply("Invalid Arguments")
 
 client.run()
