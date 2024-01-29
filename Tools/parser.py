2e3f7e66afe4863254e422302ee5f1b8e85fc385
PythonNoob999 <kerolisand@gmail.com>
2024-01-29 22:56:49 +0200

    added id

--- Tools/parser.py
+++ Tools/parser.py
@@ -33,6 +33,7 @@ KWARGS_TYPES = {
 
 
 def lnk(x):
+    if x.isdigit(): return int(x)
     try:
         return x.replace("https://t.me/","").replace("http://t.me/","").replace("https://telegram.me/","").replace("http://telegram.me/","").replace("@","")
     except:
